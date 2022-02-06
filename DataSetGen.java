2642f07797f9cb369187e4e7092b806a322834b7
ericthegoat1 <98350692+ericthegoat1@users.noreply.github.com>
2022-02-06 14:53:54 -0500

    Add files via upload

---
+++ DataSetGen.java
@@ -0,0 +1,56 @@
+/**
+   Computes the average of a set of data values.
+*/
+//Author Eric Gomez
+
+
+public class DataSetGen <E extends Measurable>
+{
+   private double sum;
+   private E maximum;
+   private int count;
+
+   /**
+      Constructs an empty data set.
+ * @return 
+   */
+   public DataSetGen()
+   {
+      sum = 0;
+      count = 0;
+      maximum = null;
+   }
+
+   /**
+      Adds a data value to the data set.
+      @param x a data value
+   */
+   
+   //defines a non static generic method
+   public void add(E x) 
+   {
+      sum = sum + x.getMeasure();
+      if (count == 0 || maximum.getMeasure() < x.getMeasure())
+         maximum = x;
+      count++;
+   }
+
+   /**
+      Gets the average of the added data.
+      @return the average or 0 if no data has been added
+   */
+   public double getAverage()
+   {
+      if (count == 0) return 0;
+      else return sum / count;
+   }
+
+   /**
+      Gets the largest of the added data.
+      @return the maximum or 0 if no data has been added
+   */
+   public Measurable getMaximum()
+   {
+      return maximum;
+   }
+}
